ca5eaa462becf36bf3f5a219765984e9eecbfa14
unnijsx <unniytman@gmail.com>
2026-01-20 16:08:44 +0530

    fix(client): update secure support link

--- client/src/utils/secureLinks.js
+++ client/src/utils/secureLinks.js
@@ -1,5 +1,5 @@
 export const getSecureSupportLink = () => {
-    // Base64 encoded: https://discord.gg/RBbFXt3YTp
-    const _0x1a2b = 'aHR0cHM6Ly9kaXNjb3JkLmdnL1JCYkZYdDNZVHA=';
+    // Base64 encoded: https://discord.gg/gyQh6KaSbp
+    const _0x1a2b = 'aHR0cHM6Ly9kaXNjb3JkLmdnL2d5UWhLS2FTYnA=';
     return atob(_0x1a2b);
 };
