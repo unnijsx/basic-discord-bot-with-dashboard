353bb1f660ccbebbb5c5462bde74cd1c3ed35825
unnijsx <unniytman@gmail.com>
2026-01-20 15:52:51 +0530

    fix(bot): add missing imports to help command

--- server/src/bot/commands/general/help.js
+++ server/src/bot/commands/general/help.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
